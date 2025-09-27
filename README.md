6e1f4c7e5c1f22c11a89da98650a03b6967bf418
Caleb Morrison <cmorrison8191@gmail.com>
2025-09-27 15:19:23 -0700

    Adjust readme and add r1 and r2 derivative tests

--- README.md
+++ README.md
@@ -45,8 +45,8 @@ post-processing.
 
 # License
 
-This project - except for the IERS data files - is covered under the Mozilla
-Public License Version 2.0 (MPL2). See the LICENSE.txt file for more
+This project - except for the IERS and WGS84 data files - is covered under the 
+Mozilla Public License Version 2.0 (MPL2). See the LICENSE.txt file for more
 information.
 
 # Acknowledgements and References
--- Tests/test_matrix_derivatives.py
+++ Tests/test_matrix_derivatives.py
@@ -3,6 +3,50 @@ import numpy as np
 from TerraFrame.Utilities import TransformationMatrices
 
 
+def test_r1_derivative():
+    n = 25
+    t = np.linspace(0, 24 * 60 * 60, n)
+    psi = [x ** 1.2 for x in t]
+    dpsidt = 1.2
+    dt = 1e-4
+    dpsi = dpsidt * dt
+
+    error = np.zeros((n,))
+
+    for i in reversed(range(n)):
+        r1 = TransformationMatrices.r1(psi[i] - dpsi)
+        r2 = TransformationMatrices.r1(psi[i] + dpsi)
+
+        drdt_e = (r2 - r1) / (2 * dt)
+        drdt = TransformationMatrices.dr1dt(psi[i], dpsidt)
+
+        error[i] = np.max(np.abs(drdt_e - drdt))
+
+    assert (max(error) < 1e-6)
+
+
+def test_r2_derivative():
+    n = 25
+    t = np.linspace(0, 24 * 60 * 60, n)
+    psi = [x ** 1.2 for x in t]
+    dpsidt = 1.2
+    dt = 1e-4
+    dpsi = dpsidt * dt
+
+    error = np.zeros((n,))
+
+    for i in reversed(range(n)):
+        r1 = TransformationMatrices.r2(psi[i] - dpsi)
+        r2 = TransformationMatrices.r2(psi[i] + dpsi)
+
+        drdt_e = (r2 - r1) / (2 * dt)
+        drdt = TransformationMatrices.dr2dt(psi[i], dpsidt)
+
+        error[i] = np.max(np.abs(drdt_e - drdt))
+
+    assert (max(error) < 1e-6)
+
+
 def test_r3_derivative():
     n = 25
     t = np.linspace(0, 24 * 60 * 60, n)
@@ -14,12 +58,12 @@ def test_r3_derivative():
     error = np.zeros((n,))
 
     for i in reversed(range(n)):
-        r1 = TransformationMatrices.r3(psi[i])
+        r1 = TransformationMatrices.r3(psi[i] - dpsi)
         r2 = TransformationMatrices.r3(psi[i] + dpsi)
 
-        drdt_e = (r2 - r1) / dt
+        drdt_e = (r2 - r1) / (2 * dt)
         drdt = TransformationMatrices.dr3dt(psi[i], dpsidt)
 
         error[i] = np.max(np.abs(drdt_e - drdt))
 
-    assert (max(error) < 1e-4)
+    assert (max(error) < 1e-6)
